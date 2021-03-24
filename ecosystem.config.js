4457db152c0901555c71ad6a8276effd2417b545
MegaMax42rus <megamax42rus@gmail.com>
2021-03-24 13:05:12 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,27 @@
-
 module.exports = {
- apps: [{
-  name: 'chill',
-  script: 'npm start',
+  apps : [{
+    name: 'cinema-chill',
+    script: 'npm start',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3003,
+    },
+  }],
 
-  // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-  instances: 1,
-  autorestart: true,
-  watch: false,
-  max_memory_restart: '1G',
-  env: {
-   SERVER: 'https://chill.dev.ctdl.space',
-      DIR: '/opt/chill',
-   NODE_ENV: 'development',
+  deploy : {
+    production : {
+      user : 'dev',
+      host : 'chill.cinema.ldtc.space',
+      ref  : 'origin/master',
+      repo : 'git@gitlab.com:cinema9/chill.git',
+      path : '/opt/chill',
+      'pre-deploy-local' : '',
+      'post-deploy' : 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'pre-setup': '',
+    }
   }
- }],
- deploy: {
-  development: {
-   'user': 'dev',
-   'host': 'chill.cinema.ldtc.space',
-   'ref': 'origin/master',
-   'repo': 'git@gitlab.com:cinema9/chill.git',
-   'path': '/opt/chill',
-   'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env development',
-  },
- },
-}
+};
