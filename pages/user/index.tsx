2029aca6de14c53aba2287a78678cfd2e3e33cf4
bratishques <gurda999@gmail.com>
2021-04-24 14:01:16 +0300

    category first iteration done

---
+++ pages/user/index.tsx
@@ -0,0 +1,16 @@
+import Footer from "../../components/footer"
+import Header from "../../components/header"
+
+const IndexPage = () => {
+    <div className="bg-background text-mainText">
+    <Header />
+    <div className="container mx-auto ">
+        <div className="max-w-screen-xl w-full mx-auto grid grid-cols-5 grid-rows-1 gap-7">
+
+        </div>
+    </div>
+    <Footer />
+</div>
+}
+
+export default IndexPage
