a5304a0a0689f7d8052f1e7bef1ba09a6551d56d
bratishques <gurda999@gmail.com>
2021-04-26 17:14:21 +0300

    Layout done

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,5 +1,18 @@
-const Layout = () => {
+import Footer from "../footer"
+import Header from "../header"
 
+const Layout = ({children}) => {
+    return (
+        <>
+        <Header />
+        <div className="container mx-auto h-full">
+            <div className="max-w-screen-xl w-full mx-auto px-6 h-full">
+            {children}
+            </div>
+        </div>
+        <Footer />
+        </>
+    )
 }
 
 export default Layout
