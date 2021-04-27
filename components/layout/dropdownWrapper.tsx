13cb18aa433c7a8a449198a0f86e0a92a82d0f10
bratishques <gurda999@gmail.com>
2021-04-27 16:38:08 +0300

    Resizable input done

--- components/layout/dropdownWrapper.tsx
+++ components/layout/dropdownWrapper.tsx
@@ -22,17 +22,25 @@ const DropdownWrapper = ({heading, children}) => {
                 sliderContainerRef.current.style.height = "0px"
             }
         })
+        window.addEventListener('autosize:resized', function(){
+            if (isSliderOpen) {
+                sliderContainerRef.current.style.height = sliderContainerRef.current.scrollHeight + "px"
+            }
+            else {
+                sliderContainerRef.current.style.height = "0px"
+            }
+          })
     },[])
     return (
-        <div className={`w-full mb-6`}>
+        <div className={`w-full mb-8`}>
             <div
                 onClick={() => {
                     window.dispatchEvent(new Event('resize'));
                     setIsSliderOpen(!isSliderOpen)
                 }}
-                className={`w-auto text-h2-mobile font-medium flex cursor-pointer`}>
-                <div className={`mr-3`}>{heading}</div>
-                <ChevronDown classname={`stroke-current w-8 h-8 transform transition-all duration-150 ease-out ${isSliderOpen ? "rotate-180" : "rotate-0"}`} />
+                className={`w-auto text-h2-mobile font-medium flex cursor-pointer ${isSliderOpen && "text-orange"}`}>
+                <div className={`mr-3 text-mainText text-h7`}>{heading}</div>
+                <ChevronDown classname={`stroke-current w-8 h-8 transform transition-all duration-150 ease-out ${isSliderOpen ? "rotate-0" : "-rotate-90"}`} />
             </div>
             <div 
             className={`w-full overflow-hidden transition-all duration-300 ease-out ${!isSliderOpen && "opacity-10"}`}
--- pages/partnership/index.tsx
+++ pages/partnership/index.tsx
@@ -4,20 +4,36 @@ import TextInput from "../../components/inputs/textInput";
 import { useState } from "react";
 import validator from "../../components/inputs/validator";
 import PartnershipHeroBlock from "../../components/partnershipHeroBlock";
+import ResizableTextInput from "../../components/inputs/resizableTextInput";
 
 
 const Partnership = () => {
     const [name, setName] = useState("")
+
     const [email, setEmail] = useState("")
     const [emailError, setEmailError] = useState(false)
 
+    const [seriesName, setSeriesName] = useState("")
+    
+    const [seriesOriginalName, setSeriesOriginalName] = useState("")
+
+    const [year, setYear] = useState("")
+
+    const [projectLink, setProjectLink] = useState("")
+
+    const [seasonAmount, setSeasonAmount] = useState("")
+
+    const [serieLength, setSerieLength] = useState("")
+
+    const [annotation, setAnnotation] = useState("")
+
     return (
         <Layout>
-        <div className={`mb-8`}>
+        <div className={`mb-16`}>
             <PartnershipHeroBlock/>
         </div>
         <form onSubmit={(e) => {e.preventDefault()}}>
-        <div className={`lg:w-2/3`}>
+        <div className={`lg:w-3/4`}>
         <DropdownWrapper heading={`Контактная информация`}>
         <div className={`w-full grid grid-cols-2 gap-14 py-6`}>
             <TextInput
@@ -41,6 +57,61 @@ const Partnership = () => {
         </div>
         </DropdownWrapper>
         <DropdownWrapper heading={`Информация о сериале`}>
+        <div className={`w-full grid grid-cols-2 gap-14 pt-6 gap-y-6`}>
+            <TextInput
+            label={'Название (На языке оригинала)'}
+            name={"series-name"}
+            placeholder={"Название (на языке оригинала)"}
+            state={seriesName}
+            setState={setSeriesName}
+            />
+            <TextInput
+            label={`Название (На английском)`}
+            name={`series-name`}
+            placeholder={`Название (на английском)`}
+            state={seriesOriginalName}
+            setState={setSeriesOriginalName}
+            />
+            <TextInput
+            label={'Год выпуска'}
+            name={"series-name"}
+            placeholder={"Введите год выпуска"}
+            state={year}
+            setState={setYear}
+            />
+            <TextInput
+            label={`Ссылка на скачивание проекта`}
+            name={`series-name`}
+            placeholder={`mp4, 16:9, H.264, HD(1920*1080); Аудио: 48 кгц, Stereo)`}
+            state={projectLink}
+            setState={setProjectLink}
+            />
+            <TextInput
+            label={'Количество сезонов и серий'}
+            name={"series-name"}
+            placeholder={"Введите количество сезонов и серий"}
+            state={seasonAmount}
+            setState={setSeasonAmount}
+            />
+            <TextInput
+            label={'Продолжительность серии'}
+            name={"series-name"}
+            placeholder={"Введите продолжительность серии"}
+            state={serieLength}
+            setState={setSerieLength}
+            />
+            <div className={`col-span-2`}>
+            <ResizableTextInput
+            label={'Аннотация'}
+            name={"annotation"}
+            placeholder={"Введите краткую аннотацию"}
+            state={annotation}
+            setState={setAnnotation}
+            />
+            </div>
+        </div>
+        </DropdownWrapper>
+        <DropdownWrapper heading={`Дополнительная информация`}>
         <div className={`w-full grid grid-cols-2 gap-14 pt-6`}>
             <TextInput
             label={'Имя'}
@@ -62,7 +133,7 @@ const Partnership = () => {
             />
         </div>
         </DropdownWrapper>
-        <DropdownWrapper heading={`Дополнительная информация`}>
+        <DropdownWrapper heading={`Социальные сети`}>
         <div className={`w-full grid grid-cols-2 gap-14 pt-6`}>
             <TextInput
             label={'Имя'}
