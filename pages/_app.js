636d3e536d0e97c4e59d60687d7fbba253fe135c
Haswell <Haswelvc@yandex.ru>
2021-04-27 15:02:55 +0700

    update _app

--- pages/_app.js
+++ pages/_app.js
@@ -3,22 +3,30 @@ import "swiper/swiper.scss"
 import "swiper/components/scrollbar/scrollbar.scss"
 import "swiper/components/pagination/pagination.scss"
 import "swiper/components/effect-fade/effect-fade.scss"
-import { useEffect } from 'react';
+import Head from 'next/head'
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
-  
   useEffect(() => {
     const appHeight = () => {
       let vh = window.innerHeight * 0.01
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
+      document.documentElement.style.setProperty("--vh", `${vh}px`)
     }
-    window.addEventListener("resize", appHeight);
-    appHeight();
+    window.addEventListener("resize", appHeight)
+    appHeight()
     return () => {
-      window.removeEventListener("resize", appHeight);
-    };
+      window.removeEventListener("resize", appHeight)
+    }
   }, [])
-  
+
+  return (
+    <div>
+      <Head>
+          <script src="//vk.com/js/api/openapi.js"></script>
+        </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
