b276c49afcdd30b959548b8fe6e5e03fd770196c
Anton <nerorimastr@gmail.com>
2021-04-29 16:21:10 +0700

    refactor: init

--- components/inputs/textInput.tsx
+++ components/inputs/textInput.tsx
@@ -16,11 +16,13 @@ const TextInput = ({label, name, type="text", placeholder="", state, setState, v
     return (
         <label className="block w-full font-medium">
             {label}
-            <input type={type} name={name} className={`border-2  rounded-lg sm:px-8 py-4 px-4 sm:py-6 mt-3 focus:outline-none bg-popupBackground w-full text-ui-text ${error ? "border-error-red" : "focus:border-orange hover:border-orange border-popupBorder"}  transition-all duration-200 ease-out`} placeholder={placeholder} value={state} onChange={(e) => {
+            <input type={type} name={name} className={`border-2  rounded-lg sm:px-8 py-4 px-4 sm:py-6 mt-3 focus:outline-none bg-popupBackground w-full text-ui-text ${error ? "border-error-red" : "focus:border-orange hover:border-orange border-popupBorder"}  transition-all duration-200 ease-out`} 
+            placeholder={placeholder} 
+            value={state} 
+            onChange={(e) => {
                 setState(e.target.value)
                 setError(!validator(e.target.value))
             }} ref={inputRef}>
-
             </input>
             <div className={`flex items-center mt-2 ${!error && "hidden"}`}>
             <img src="/icons/warning.svg" className={`flex-shrink-0 w-5 h-5 mr-1`} alt=""/>
