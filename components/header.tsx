7e261447f6ef8e154b113cb40e746fec975ab8c5
Anton <nerorimastr@gmail.com>
2021-04-29 19:01:33 +0700

    refactor: init

--- components/header.tsx
+++ components/header.tsx
@@ -74,6 +74,7 @@ export default function Header() {
             </div>
             <div className="flex justify-between items-center sm:py-6 md:space-x-10">
               {router.pathname.match(/^\/films\/+/) !== null ?
+                <a href="/">
                 <svg
                   width="35"
                   height="35"
@@ -90,6 +91,7 @@ export default function Header() {
                     strokeLinejoin="round"
                   />
                 </svg>
+                </a>
                 :
                 <div className="flex justify-center sm:hidden">
                   <a href="/">
@@ -112,12 +114,12 @@ export default function Header() {
               </nav>
             </div>
             <nav className="flex justify-end">
-              <a href="#" onClick={openSearch} className="text-base text-black-500 hover:text-orange ml-5 w-6 h-6 sm:h-10 sm:w-10">
+              {/* <a href="#" onClick={openSearch} className="text-base text-black-500 hover:text-orange ml-5 w-6 h-6 sm:h-10 sm:w-10">
                 <svg className={`w-full h-full`} width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M12.9545 3.75C11.134 3.75 9.35443 4.28983 7.84075 5.30124C6.32708 6.31264 5.14732 7.75018 4.45065 9.43209C3.75399 11.114 3.57171 12.9647 3.92687 14.7502C4.28202 16.5357 5.15867 18.1758 6.44594 19.4631C7.73321 20.7503 9.37329 21.627 11.1588 21.9821C12.9443 22.3373 14.795 22.155 16.4769 21.4583C18.1588 20.7617 19.5964 19.5819 20.6078 18.0682C21.6192 16.5546 22.159 14.775 22.159 12.9545C22.1588 10.5134 21.189 8.17225 19.4629 6.44611C17.7367 4.71996 15.3956 3.75016 12.9545 3.75V3.75Z" stroke="white" strokeWidth="2" strokeMiterlimit="10" />
                   <path d="M19.8218 19.8217L26.2501 26.25" stroke="white" strokeWidth="2" strokeMiterlimit="10" strokeLinecap="round" />
                 </svg>
-              </a>
+              </a> */}
               <a className={`${!loginContext.userToken ? "hidden" : ""} text-base hover:text-gray-900 ml-5 w-6 h-6 sm:h-10 sm:w-10 text-orange`} href={`/user`}>
                 <svg className={`w-full h-full stroke-current`} width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M15 18.75C19.1421 18.75 22.5 15.3921 22.5 11.25C22.5 7.10786 19.1421 3.75 15 3.75C10.8579 3.75 7.5 7.10786 7.5 11.25C7.5 15.3921 10.8579 18.75 15 18.75Z" strokeWidth="2" strokeMiterlimit="10" />
