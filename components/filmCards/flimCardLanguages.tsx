e302d05f01f4c7fdbc7365dfd19fadcb7008555a
Haswell <Haswelvc@yandex.ru>
2021-05-05 20:37:42 +0700

    bug fix

--- components/filmCards/flimCardLanguages.tsx
+++ components/filmCards/flimCardLanguages.tsx
@@ -1,13 +1,11 @@
 const FilmCardLanguages = ({ data }) => {
   return (
     <div className="absolute bottom-0 left-0 h-8 sm:h-12 mx-auto w-auto grid grid-flow-col gap-2 flex-wrap">
-
       {data.localization.map((language, i) => (
-                <a className={`relative z-10`} href={`/films/${data.stringName}?lang=${language ? language.toUpperCase() : ""}`}>
-          <a
-            className={`relative z-10`}
-                        {language ? language.toUpperCase() : ""}
-          ></a>
+        <a
+          className={`relative z-10`}
+          href={`/films/${data.stringName}?lang=${language ? language.toUpperCase() : ""}`}
+        >
           <a className={`relative z-10`} href={`/films/${data.stringName}?lang=${language.toLowerCase()}`}>
             <div
               key={i}
@@ -16,7 +14,7 @@ const FilmCardLanguages = ({ data }) => {
               <h1 className="text-md sm:text-lg font-roboto font-medium text-mainText">{language.toUpperCase()}</h1>
             </div>
           </a>
-        </div>
+        </a>
       ))}
     </div>
   )
