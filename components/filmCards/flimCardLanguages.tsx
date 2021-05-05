631e8eb1452783be7a7ea569b36a4a9a9f8ad1ba
Viktor Tolstov <viktor.tolstov.tsu@gmail.com>
2021-05-05 11:26:00 +0700

    deploy fix

--- components/filmCards/flimCardLanguages.tsx
+++ components/filmCards/flimCardLanguages.tsx
@@ -2,7 +2,7 @@ const FilmCardLanguages = ({data}) => {
     return (
         
         <div className="absolute bottom-0 left-0 h-8 sm:h-12 mx-auto w-auto grid grid-flow-col gap-2 flex-wrap">
-        {data.localization.map((language, i) => {    
+        {data.localization ? data.localization.map((language, i) => {    
             return (
                 <a className={`relative z-10`} href={`/films/${data.stringName}?lang=${language.value.toLowerCase()}`}>
                 <div key={i} className="h-8 sm:h-12 w-8 sm:w-12 flex flex-wrap content-center bg-filmInfoBackground justify-center relative z-10">
@@ -12,7 +12,7 @@ const FilmCardLanguages = ({data}) => {
                 </div>
                 </a>
             )
-        })}
+        }) : "" }
     </div>
     )
 }
