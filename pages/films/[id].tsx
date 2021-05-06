bf00e68fc65e7fd675422b06bbc147c079f7e998
Anton <nerorimastr@gmail.com>
2021-05-06 12:15:36 +0700

    refactor: init

--- pages/films/[id].tsx
+++ pages/films/[id].tsx
@@ -45,19 +45,22 @@ export default function IndexPage({ movie, playlist, movies }) {
   const [score, setscore] = useState(null)
 
   console.log(movie)
+
   const series = []
-  for (let season in movie.serial) {
-    const seasonBuffer = []
-    for (let serie in movie.serial[season].series) {
-      seasonBuffer.push({
-        videoId: movie.serial[season].series[serie].value,
-        acting: "LostFilm",
-        videoLength: "10",
-        image: movie.image,
-      })
+
+  if (movie.serial)
+    for (let season in movie.serial) {
+      const seasonBuffer = []
+      for (let serie in movie.serial[season].series) {
+        seasonBuffer.push({
+          videoId: movie.serial[season].series[serie].value,
+          acting: "LostFilm",
+          videoLength: "10",
+          image: movie.image,
+        })
+      }
+      series.push(seasonBuffer)
     }
-    series.push(seasonBuffer)
-  }
 
 
   return (
@@ -88,10 +91,10 @@ export default function IndexPage({ movie, playlist, movies }) {
           </a>
         </nav>
 
-      <Video name={movie.title} series={series} movieId = {movie._id} movies = {movies}/>
-      <div className={`hidden sm:block`}>
-        <ReviewsAndLikes _likes={movie._likes} _dislikes={movie._dislikes} score={score} setscore={setscore} />
-      </div>
+        <Video name={movie.title} series={series} movieId = {movie._id} movies = {movies}/>
+        <div className={`hidden sm:block`}>
+          <ReviewsAndLikes _likes={movie._likes} _dislikes={movie._dislikes} score={score} setscore={setscore} />
+        </div>
         <Series series={series} />
 
         <FilmDescription
