4c8b11eec8b81487eba84348b9dd3d65af02bd36
Haswell <Haswelvc@yandex.ru>
2021-05-12 12:23:17 +0700

    fix mnu

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -179,13 +179,13 @@ export default function Header() {
                     xmlns="http://www.w3.org/2000/svg"
                     className="inline sm:hidden"
                   >
-                    <path
+                    {/* <path
                       d="M10.8906 13.2812L6.10937 8.5L10.8906 3.71875"
                       stroke="#FFFFFF"
                       strokeWidth="1"
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                    />
+                    /> */}
                   </svg>
                 </a>
                 <div className="bg-filmReviewBackground flex font-medium rounded-lg sm:px-4 mt-4 py-2 px-2 sm:py-4 w-full text-ui-text transition-all duration-200 ease-out">
@@ -230,7 +230,30 @@ export default function Header() {
                 {/* >640px */}
                 <HeadPC />
               </div>
-              <nav
+              <nav className="flex justify-end mt-6 sm:mt-0 relative z-20">
+              <a
+                href="/search"
+                onClick={openSearch} className="text-base text-black-500 hover:text-orange ml-5 w-6 h-6 sm:h-8 sm:w-8">
+                <svg className={`w-full h-full`} width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12.9545 3.75C11.134 3.75 9.35443 4.28983 7.84075 5.30124C6.32708 6.31264 5.14732 7.75018 4.45065 9.43209C3.75399 11.114 3.57171 12.9647 3.92687 14.7502C4.28202 16.5357 5.15867 18.1758 6.44594 19.4631C7.73321 20.7503 9.37329 21.627 11.1588 21.9821C12.9443 22.3373 14.795 22.155 16.4769 21.4583C18.1588 20.7617 19.5964 19.5819 20.6078 18.0682C21.6192 16.5546 22.159 14.775 22.159 12.9545C22.1588 10.5134 21.189 8.17225 19.4629 6.44611C17.7367 4.71996 15.3956 3.75016 12.9545 3.75V3.75Z" stroke="white" strokeWidth="1" strokeMiterlimit="10" />
+                  <path d="M19.8218 19.8217L26.2501 26.25" stroke="white" strokeWidth="1" strokeMiterlimit="10" strokeLinecap="round" />
+                </svg>
+              </a>
+              <div onClick={() => {
+                if (loginContext.userToken) {
+                  router.push("/user")
+                }
+                else {
+                  authModalContext.setModalOpen(true)
+                }
+                }} className={`w-6 h-6 sm:h-8 sm:w-8 hidden sm:block ${!loginContext.userToken ? "text-mainText" : "text-orange"} cursor-pointer ml-6`}>
+              <UserIcon/>
+              </div>
+              <div onClick={() => {setIsSidebarOpen(!isSidebarOpen)}} className={`sm:hidden ml-5 w-6 h-6 sm:h-8 sm:w-8 cursor-pointer ${isSidebarOpen ? 'text-orange' : "text-mainText"}`}>
+              <MenuIcon isSidebarOpen={!isSidebarOpen}/>
+              </div>
+            </nav>
+              {/* <nav
                 className={`
             ${
               router.pathname.split("/")[1] === "search" ? "hidden" : "hidden sm:flex"
@@ -288,7 +311,7 @@ export default function Header() {
                 >
                   <MenuIcon isSidebarOpen={!isSidebarOpen} />
                 </div>
-              </nav>
+              </nav> */}
             </div>
           </div>
           <AuthWindow
