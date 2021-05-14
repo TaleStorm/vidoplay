0ee1029bb2ba4b302fc23b90441d31baa1a582ff
Anton <nerorimastr@gmail.com>
2021-05-15 01:35:17 +0700

    add: button

---
+++ components/wantToSeeCard.tsx
@@ -0,0 +1,120 @@
+import { useState } from "react"
+
+type mockupType = {
+  image: string,
+  title: string,
+  description: string,
+  year: number,
+  genre: Array<string>,
+  country: string,
+  serias: string
+}
+
+export default function WantToSeeCard(data: mockupType) {
+
+  const [voiceIsCounted, setVoiceIsCounted] = useState<boolean>(false)
+
+  return (
+    <div className=" mb-4 rounded-lg p-4 bg-cardBackground">
+      <div className="flex items-stretch">
+          <img className="h-32 sm:h-auto  rounded-lg" 
+          // style={{ maxHeight: 244 }} 
+          src={data.image} />
+        <div className="grid grid-cols-1 ml-5 relative" style={{ gridTemplateRows: "auto 1fr auto" }}>
+          <div className="row-span-1 flex justify-between mb-1 sm:mb-2">
+            <h1 className="text-mainText text-md font-medium sm:text-2xl overflow-hidden">
+              {data.title}
+            </h1>
+            <h1 className="hidden sm:block text-dramaTag text-xl" style={{ minWidth: 110 }}>
+              {data.serias}
+            </h1>
+          </div>
+          <div className="text-secondaryText hidden sm:grid grid-cols-12">
+            <div className="col-span-10">
+              {data.description}
+            </div>
+          </div>
+          <div className="row-span-1 flex sm:datas-end w-full justify-between">
+            <div className="text-sm sm:text-lg font-medium">
+              <div className="flex">
+                <h1 className="mr-2 text-secondaryText">Год:</h1>
+                <h1 className="text-mainText">{data.year}</h1>
+              </div>
+              <div className="flex">
+                <h1 className="mr-2 text-secondaryText">Жанр:</h1>
+                <h1 className="text-mainText">{data.genre.join(", ")}</h1>
+              </div>
+              <div className="flex">
+                <h1 className="mr-2 text-secondaryText">Германия:</h1>
+                <h1 className="text-mainText">{data.country}</h1>
+              </div>
+            </div>
+            <div className="hidden lg:flex items-end">
+              {voiceIsCounted ? (<button
+                className={`flex px-6 justify-center text-white transition-colors duration-300 hover:bg-voice-button-voted-hover bg-voice-button-voted p-3 rounded-lg w-full`}
+                onClick={() => { setVoiceIsCounted(!voiceIsCounted) }}
+              >
+                <div className="flex items-center">
+                  <p className="mr-1 whitespace-nowrap">
+                    Ваш голос учтён
+                      </p>
+                  <svg className="h-6" width="30" height="29" viewBox="0 0 30 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M26 7L12 21L5 14.0003" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                </div>
+              </button>) : (
+                <button
+                  className={`flex px-6 justify-center text-white transition-colors duration-300 hover:bg-button-hover bg-orange p-3 rounded-lg w-full`}
+                  onClick={() => { setVoiceIsCounted(!voiceIsCounted) }}
+                >
+                  <div className="flex items-center">
+
+                    <p className="mr-1 whitespace-nowrap">
+                      Хочу увидеть на Chill
+                      </p>
+                    <svg className="h-6" width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M14.2856 4.28564V24.2856" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                      <path d="M4.28564 14.2856H24.2856" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                  </div>
+                </button>)}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-secondaryText text-sm mt-2 sm:hidden" style={{ minHeight: 100 }}>
+        {data.description}
+      </div>
+      {voiceIsCounted ? (<button
+        className={`lg:hidden flex mt-3 px-6 justify-center text-white transition-colors duration-300 hover:bg-voice-button-voted-hover bg-voice-button-voted p-3 rounded-lg w-full`}
+        onClick={() => { setVoiceIsCounted(!voiceIsCounted) }}
+      >
+        <div className="flex items-center">
+          <p className="mr-1 whitespace-nowrap">
+            Ваш голос учтён
+          </p>
+          <svg className="h-6" width="30" height="29" viewBox="0 0 30 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M26 7L12 21L5 14.0003" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </div>
+      </button>) : (
+        <button
+          className={`lg:hidden flex mt-3 px-6 justify-center text-white transition-colors duration-300 hover:bg-button-hover bg-orange p-3 rounded-lg w-full`}
+          onClick={() => { setVoiceIsCounted(!voiceIsCounted) }}
+        >
+          <div className="flex items-center">
+
+            <p className="mr-1 whitespace-nowrap">
+              Хочу увидеть на Chill
+                      </p>
+            <svg className="h-6" width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M14.2856 4.28564V24.2856" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M4.28564 14.2856H24.2856" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+          </div>
+        </button>)}
+    </div>
+  )
+}
+
+
--- pages/wanttosee/index.tsx
+++ pages/wanttosee/index.tsx
@@ -8,6 +8,7 @@ import useDebounce from "../../components/hooks/useDebounce"
 import Checkbox from "../../components/inputs/checkbox"
 import CheckboxDropdown from "../../components/inputs/checkboxDropdown"
 import Dropdown from "../../components/inputs/dropdown"
+import WantToSeeCard from "../../components/wantToSeeCard"
 import apiReq from "../../services/api-requests"
 
 const ApiReq = new apiReq()
@@ -37,76 +38,8 @@ export default function IndexPage({ }) {
 
   return (
     <div className="w-full">
-      {films.map((item: mockupType) =>
-        <div className=" mb-4 rounded-lg p-4 bg-cardBackground">
-          <div className="flex">
-            <img className="h-32 sm:h-full  rounded-lg" style={{ maxHeight: 244 }} src={item.image} />
-            <div className="grid grid-cols-1 ml-5 relative" style={{ gridTemplateRows: "auto 1fr auto" }}>
-              <div className="row-span-1 flex justify-between mb-1 sm:mb-2">
-                <h1 className="text-mainText text-md font-medium sm:text-2xl overflow-hidden">
-                  {item.title}
-                </h1>
-                <h1 className="hidden sm:block text-dramaTag text-xl" style={{ minWidth: 110 }}>
-                  {item.serias}
-                </h1>
-              </div>
-              <div className="text-secondaryText hidden sm:grid grid-cols-12">
-                <div className="col-span-10">
-                  {item.description}
-                </div>
-              </div>
-              <div className="row-span-1 flex sm:items-end w-full justify-between">
-                <div className="text-sm sm:text-lg font-medium">
-                  <div className="flex">
-                    <h1 className="mr-2 text-secondaryText">Год:</h1>
-                    <h1 className="text-mainText">{item.year}</h1>
-                  </div>
-                  <div className="flex">
-                    <h1 className="mr-2 text-secondaryText">Жанр:</h1>
-                    <h1 className="text-mainText">{item.genre.join(", ")}</h1>
-                  </div>
-                  <div className="flex">
-                    <h1 className="mr-2 text-secondaryText">Германия:</h1>
-                    <h1 className="text-mainText">{item.country}</h1>
-                  </div>
-                </div>
-                <div className="hidden sm:flex items-end">
-                  <button
-                    className="flex px-6 justify-center transition-colors duration-300 hover:bg-button-hover text-white bg-orange p-3 rounded-lg w-full"
-                    onClick={() => { }}
-                  >
-                    <div className="flex items-center">
-                      <p className="mr-1 whitespace-nowrap">
-                        Хочу увидеть на Chill
-                    </p>
-                      <svg className="h-6" width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M14.2856 4.28564V24.2856" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M4.28564 14.2856H24.2856" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-                      </svg>
-                    </div>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="text-secondaryText text-sm mt-2 sm:hidden">
-            {item.description}
-          </div>
-          <button
-            className="sm:hidden flex mt-3 px-6 justify-center transition-colors duration-300 hover:bg-button-hover text-white bg-orange p-3 rounded-lg w-full"
-            onClick={() => { }}
-          >
-            <div className="flex items-center">
-              <p className="mr-1">
-                Хочу увидеть на Chill
-                    </p>
-              <svg className="h-6" width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M14.2856 4.28564V24.2856" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M4.28564 14.2856H24.2856" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
-            </div>
-          </button>
-        </div>
+      {films.map((item: mockupType,i) =>
+        <WantToSeeCard key={i} {...item}/>
       )}
     </div>
   )
