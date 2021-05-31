8b524fbbdd04aecaed459661add5622a190ae8fa
Haswell <Haswelvc@yandex.ru>
2021-05-31 19:40:50 +0700

    fix 500

--- next.config.js
+++ next.config.js
@@ -37,7 +37,9 @@ const nextConfig = {
       'chillvision.ru',
       "hsto.org",
       's-ed1.cloud.gcore.lu',
-      "habrastorage.org"
+      "habrastorage.org",
+      "chillvision.gcdn.co",
+      "media.gcorelabs.com"
     ],
   },
 }
