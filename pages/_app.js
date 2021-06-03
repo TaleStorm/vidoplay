0f20c5e1f12fba53714272b2656a238071d46fa2
bratishques <gurda999@gmail.com>
2021-06-03 19:18:02 +0300

    fix:title added

--- pages/_app.js
+++ pages/_app.js
@@ -65,6 +65,7 @@ function MyApp({ Component, pageProps, token }) {
                   <PlayerEventsContextProvider>
                   <CatalogContextProvider>
                   <Head>
+                  <title>Веб-кинотеатр CHILL</title>
                     <script src="//vk.com/js/api/openapi.js"></script>
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
