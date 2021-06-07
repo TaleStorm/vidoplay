0d48e36f78a89399441b5ebaa8d4a34f3e6f06f2
Anton <nerorimastr@gmail.com>
2021-06-07 14:40:20 +0700

    fix: metric function

--- pages/_app.js
+++ pages/_app.js
@@ -59,12 +59,12 @@ function MyApp({ Component, pageProps, token }) {
         function () {
           ;(m[i].a = m[i].a || []).push(arguments)
         }
-      ;(m[i].l = 1 * new Date()((k = e.createElement(t)))),
+      m[i].l = 1 * new Date()
+      ;(k = e.createElement(t)),
         (a = e.getElementsByTagName(t)[0]),
         (k.async = 1),
         (k.src = r),
         a.parentNode.insertBefore(k, a)
-      console.log("ymMetrickInit")
     })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym")
 
     ym(64534015, "init", {
